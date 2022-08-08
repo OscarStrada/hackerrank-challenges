9e4cec440c6ce7af2bd024d91a1a8ef9b696f5d8
Oscar Estrada <arturostrada2020@gmail.com>
2022-08-08 09:21:37 -0500

    refactor sales by match solution

--- salesByMatch.js
+++ salesByMatch.js
@@ -1,44 +1,14 @@
 function sockMerchant(n, ar) {
-  const arrLength = n;
-  const counters = [];
-  let models = [];
-  let output = 0;
+  // Delete duplicate array items
+  const uniqueValues = [...new Set(ar)];
 
-  // Delete duplicate items from the array
-  const uniqueAr = ar.filter((item, index) => {
-    return ar.indexOf(item) === index;
+  let count = 0;
+  uniqueValues.forEach((value) => {
+    const filterValues = ar.filter((element) => element == value);
+    const pairsNumber = Math.floor(filterValues.length / 2);
+    count += pairsNumber;
   });
-
-  // Sort elements from the array
-  function compareNumeric(a, b) {
-    if (a > b) return 1;
-    if (a == b) return 0;
-    if (a < b) return -1;
-  }
-
-  // function for filter an array
-  function filterArr(array, compare) {
-    const model = array.filter((x) => x === compare);
-    return model;
-  }
-
-  models = uniqueAr.sort(compareNumeric);
-
-  if (arrLength === ar.length) {
-    for (let i = 0; i < models.length; i++) {
-      arrFilterModel = filterArr(ar, models[i]);
-
-      if (arrFilterModel.length % 2 !== 0) {
-        counters.push(arrFilterModel.length - 1);
-      } else {
-        counters.push(arrFilterModel.length);
-      }
-    }
-
-    output = counters.reduce((a, b) => a + b, 0) / 2;
-  } else throw new Error("The ar length must be iqual to n");
-
-  return output;
+  return count;
 }
 
 const res = sockMerchant(9, [10, 20, 20, 10, 10, 30, 50, 10, 20]);

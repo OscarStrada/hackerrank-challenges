1e853d2821e5a314d066d6aa659dbad7263d8870
Oscar Estrada <arturostrada2020@gmail.com>
2022-08-08 09:15:09 -0500

    refactor grading students solution

--- gradingStudents.js
+++ gradingStudents.js
@@ -1,20 +1,8 @@
-function gradingStudents(grades = []) {
-  let output = [];
-  for (let i = 0; i < grades.length; i++) {
-    let newArr = grades[i];
-    console.log(newArr);
-
-    if (grades[i] >= 0 && grades[i] <= 100) {
-      if (grades[i] >= 38 && grades[i] % 5 >= 3) {
-        const res = grades[i] - (grades[i] % 5) + 5;
-        output.push(res);
-      } else {
-        output.push(grades[i]);
-      }
-    } else output.push("error");
-  }
-
-  return output;
+function gradingStudents(grades) {
+  let finalGrades = grades.map((grade) => {
+    return grade >= 38 && grade % 5 >= 3 ? grade - (grade % 5) + 5 : grade;
+  });
+  return finalGrades;
 }
 
 res = gradingStudents([73, 38, 33, 67, 110]);
